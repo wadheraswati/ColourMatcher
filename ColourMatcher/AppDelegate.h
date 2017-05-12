4933f80e399fd4923ef73a29d29f80a485fcb0e9
wadheraswati <wadheraswati@gmail.com>
2017-05-12 12:11:20 +0530

    Initial Commit

---
+++ ColourMatcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColourMatcher
+//
+//  Created by Swati Wadhera on 5/12/17.
+//  Copyright © 2017 Swati Wadhera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
